94c7295fa526bc820fa89202bb13e0dcd647b98d
Thomas Boerger <tboerger@tbpro.de>
2013-10-18 16:04:57 +0200

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name "sshkey"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures sshkey"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+recipe "sshkey", "Installs/Configures sshkey"
+
+supports "suse"
---
+++ providers/default.rb
@@ -0,0 +1,66 @@
+#
+# Cookbook Name:: sshkey
+# Provider:: default
+#
+# Copyright 2013, Thomas Boerger
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require "chef/dsl/include_recipe"
+include Chef::DSL::IncludeRecipe
+
+action :create do
+  directory "#{home_directory}/.ssh" do
+    mode 0700
+    owner new_resource.username
+    group new_resource.username
+  end
+
+  template "#{home_directory}/.ssh/authorized_keys" do
+    mode 0600
+    owner new_resource.username
+    group new_resource.username
+
+    cookbook "sshkey"
+    source "authorized_keys.conf.erb"
+
+    variables(
+      "keys" => new_resource.keys
+    )
+  end
+
+  new_resource.updated_by_last_action(true)
+end
+
+action :delete do
+  file "#{home_directory}/.ssh/authorized_keys" do
+    action :delete
+  end
+
+  new_resource.updated_by_last_action(true)
+end
+
+protected
+
+def home_directory
+  if new_resource.home
+    new_resource.home
+  else
+    if new_resource.username == "root"
+      "/root"
+    else
+      "/home/#{new_resource.username}"
+    end
+  end
+end
